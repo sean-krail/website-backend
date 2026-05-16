ab0bd3de2b3a578dbb039585f20c1e8e3ef68895
Sean Krail <sean@krail.dev>
2026-05-16 14:23:15 +0000

    fix(devcontainer): correct remoteUser, debian-base, latest node, latest rust

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-corepack enable
-corepack install
 pnpm install
 curl -fsSL https://cargo-lambda.info/install.sh | sh
 cargo install cargo-edit
 pnpm format
 pnpm build
 pnpm test
+pnpm cdk synth
